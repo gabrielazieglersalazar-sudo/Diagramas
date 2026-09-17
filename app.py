a45d51b1767ed16d793e107a069740f1afb2654f
Gabriela Ziegler <zieglergabriela21@gmail.com>
2026-09-17 15:54:53 -0400

    agregando fotos

--- app.py
+++ app.py
@@ -15,8 +15,6 @@ with col1:
 
 with col2:
     st.markdown("<h1 style='text-align: center; color: #003366;'>Diagramas de la Planta</h1>", unsafe_allow_html=True)
-    
-    # Aquí agregamos los autores y tutores con una fuente diferente (Georgia) y tamaño ajustado
     st.markdown("""
         <p style='text-align: center; color: #333333; font-family: "Georgia", serif; font-size: 18px;'>
             <b>Realizado por:</b> E. Nicole Palencia, Marie Paternina y Gabriela Ziegler.<br>
@@ -35,7 +33,6 @@ st.markdown("---")
 # --- FUNCIÓN PARA MOSTRAR PDFs ---
 def mostrar_pdf(ruta_archivo):
     try:
-        # Usamos el nuevo componente seguro para renderizar PDFs
         pdf_viewer(ruta_archivo)
     except Exception:
         st.error(f"❌ Ocurrió un error al intentar mostrar el archivo: {ruta_archivo}")
@@ -44,10 +41,8 @@ def mostrar_pdf(ruta_archivo):
 st.subheader("1. Diagramas de Bloques de Procesos (DBP)")
 archivo_dbp = "Diagramas - Palencia,Paternina,Ziegler-DBP.drawio.pdf"
 
-# Mostrar visor
 mostrar_pdf(archivo_dbp)
 
-# Botón de descarga
 try:
     with open(archivo_dbp, "rb") as f:
         st.download_button(label="📥 Descargar DBP (Opcional)", data=f, file_name=archivo_dbp, mime="application/pdf", key="btn_dbp")
@@ -81,3 +76,18 @@ try:
         st.download_button(label="📥 Descargar DFP Final (Opcional)", data=f, file_name=archivo_dfp, mime="application/pdf", key="btn_dfp")
 except FileNotFoundError:
     pass
+
+
+# --- CIERRE Y AGRADECIMIENTO ---
+st.markdown("---")
+st.markdown("<h2 style='text-align: center; color: #003366; font-family: \"Georgia\", serif;'>¡Gracias por asistir!</h2>", unsafe_allow_html=True)
+
+# Usamos 3 columnas para que la foto quede perfectamente centrada en el medio
+espacio_izq, col_foto, espacio_der = st.columns([1, 2, 1])
+
+with col_foto:
+    try:
+        # Aquí cambiamos la palabra por use_container_width
+        st.image("nosotras.png", use_container_width=True)
+    except FileNotFoundError:
+        st.warning("Foto de cierre no encontrada")
